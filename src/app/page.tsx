c5d8323f865fe6201c0d5b11b5daf6899750ec29
ajaydarisi <dgvsak7@gmail.com>
2024-11-23 23:49:25 +0530

    - adjust start button ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import welcome_icon from "../../public/welcome_icon-removebg-preview.png";
 
 export default function Home() {
     return (
-        <div className="h-screen bg-[linear-gradient(to_bottom,_#000000,_#00007b)] flex flex-col items-center justify-center">
+        <div className="h-screen relative bg-[linear-gradient(to_bottom,_#000000,_#00007b)] flex flex-col items-center justify-center">
             <div className="flex justify-center text-5xl font-bold text-white mb-[50px]">
                 {"Welcome!".split("").map((letter, index) => (
                     <span
@@ -19,10 +19,9 @@ export default function Home() {
             <Image
                 src={welcome_icon}
                 alt="Welcome Icon"
-                className="h-[200px] mx-auto"
             />
-            <div className="b-1 font-bold text-white bg-green-500 h-[50px] w-[120px] rounded-[20px] mt-[40px] flex items-center justify-center cursor-pointer">
-                <Link href="/MobileVerification">START</Link>
+            <div className="b-1 font-bold text-white absolute bottom-80 bg-green-500 h-[50px] w-[120px] rounded-[20px] mt-[40px] flex items-center justify-center cursor-pointer">
+                <Link href="/MobileVerification"><div>START</div></Link>
             </div>
         </div>
     );
